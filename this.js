165ad4b6389da894d1b9d539576e8a2940a3e77c
Aakanksha03Bamniya <aakankshabamniya03@gmail.com>
2024-05-30 23:56:20 +0530

    expense_tracker

--- this.js
+++ this.js
@@ -58,3 +58,15 @@ obj1.sale.call(obj2)
 // THIS AND APPLY KEYWORD
 
 obj1.sale.apply([obj2])
+
+
+//THIS AND BIND KEYWORD 
+// bind is used to invoke a funcation later means , its just used to create a fun but is can call by anyother project anytime
+
+
+
+// Bind the sale method of obj1 to obj2
+let boundSale = obj1.sale.bind(obj2);
+
+// Call the bound function
+boundSale(); // Outputs: Hi, we are selling gold and its price for 10 grams is 2100
